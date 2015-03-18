fa385a3a18514b6fa4cf7a0f4197ab6b0e64bf08
pablo14 <pcasas.biz@gmail.com>
2015-03-18 14:50:14 -0300

    changes

--- eu_maps.R
+++ eu_maps.R
@@ -6,6 +6,14 @@ library(markdown)
 ## Read data (this data has already some data preparation)
 data=read.delim("data.txt", header=TRUE)
 
+## Get country cod-name to translate abbreviations
+country_name_abb=read.csv("country_name_abb.txt", header=TRUE)
+data=merge(data, country_name_abb, by="Country", all.x =T)
+
+## Get clean Country variable
+data$Country=data$Name
+
+
 ## Calculate average to consider both genders
 data_avg_all=ddply(data, c("Variable", "Country"), .fun = function(d)
   c(
@@ -64,10 +72,6 @@ map_3<<-gvisGeoChart(data_merged, locationvar="Country",
 #####################################
 ## Top 3 countries (healty and expectancy)
 ######################################
-## Get country cod-name to translate abbreviations
-country_name_abb=read.csv("country_name_abb.txt", header=TRUE)
-
-data_merged=merge(data_merged, country_name_abb, by="Country", all.x =T)
 
 
 ## Order data for both variables, and keeping the highest top 3
@@ -75,8 +79,8 @@ top3_hea_lif=head(data_merged[order(data_merged$Healthy_Life, decreasing=c(T)),]
 top3_lif_exp=head(data_merged[order(data_merged$Life_Expectancy, decreasing=c(T)),],3) 
 
 ## Tables building
-table_top3_hea_lif <<- gvisTable(top3_hea_lif[,c("Name","Healthy_Life" )])
-table_top3_lif_exp <<- gvisTable(top3_lif_exp[,c("Name","Life_Expectancy" )])
+table_top3_hea_lif <<- gvisTable(top3_hea_lif[,c("Country","Healthy_Life" )])
+table_top3_lif_exp <<- gvisTable(top3_lif_exp[,c("Country","Life_Expectancy" )])
 
 tables_top3 <<- gvisMerge(table_top3_hea_lif, table_top3_lif_exp, horizontal=TRUE) 
 
@@ -88,8 +92,8 @@ top3_highest_gap=head(data_merged[order(data_merged$age_gap, decreasing=c(T)),],
 top3_lowest_exp=head(data_merged[order(data_merged$age_gap),],3) 
 
 ## Tables building
-table_top3_gap_hi <<- gvisTable(top3_highest_gap[,c("Name","age_gap" )])
-table_top3_gap_lo <<- gvisTable(top3_lowest_exp[,c("Name","age_gap" )])
+table_top3_gap_hi <<- gvisTable(top3_highest_gap[,c("Country","age_gap" )])
+table_top3_gap_lo <<- gvisTable(top3_lowest_exp[,c("Country","age_gap" )])
 
 tables_top3_gap <<- gvisMerge(table_top3_gap_hi, table_top3_gap_lo, horizontal=TRUE) 
 
